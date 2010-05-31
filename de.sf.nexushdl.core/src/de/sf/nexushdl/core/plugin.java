1c0869a25dbe7f854085c822af888d59d27d0e62
free_solo <free_solo@21d350d1-1d7f-4d78-8823-8ff9196eb808>
2010-05-31 20:56:15 +0000

    Initial Version. Invoke "kate" :-)

---
+++ de.sf.nexushdl.core/src/de/sf/nexushdl/core/plugin.java
@@ -0,0 +1,50 @@
+package de.sf.nexushdl.core;
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class plugin extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "de.sf.nexushdl.core";
+
+	// The shared instance
+	private static plugin plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public plugin() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static plugin getDefault() {
+		return plugin;
+	}
+
+}
