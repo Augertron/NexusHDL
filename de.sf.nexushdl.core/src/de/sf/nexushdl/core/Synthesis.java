8d4aa5e8a07b677253e9fe7de9d973cef036d1a7
free_solo <free_solo@21d350d1-1d7f-4d78-8823-8ff9196eb808>
2010-06-20 11:36:42 +0000

    First working XST version

--- de.sf.nexushdl.core/src/de/sf/nexushdl/core/Synthesis.java
+++ de.sf.nexushdl.core/src/de/sf/nexushdl/core/Synthesis.java
@@ -22,8 +22,10 @@
 package de.sf.nexushdl.core;
 
 import java.io.*;
+import java.util.Iterator;
 
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 
 /**
@@ -40,13 +42,21 @@ public class Synthesis {
 	}
 
 	private IResource	root = null;			///< The root resource that is that will be starting point for doxyfiles search.
+	private IResource	m_hdlfile;
+	private String		m_hdlfile_extension;
+	private String		m_hdlfile_name;
+	private String		m_hdlfile_path;
+	private String		m_hdlfile_type;
+	private String		m_hdlfile_ws_path;
 	
+
 	/**
 	 * Run Xilinx XST for synthesis
 	 * 
 	 * @throws CoreException, InvokeException 
+	 * @throws IOException 
 	 */
-	public void Use_XST () throws CoreException, InvokeException {
+	public void Use_XST () throws CoreException, InvokeException, IOException {
 		try {
 			ResourceCollector	collector = root != null ? ResourceCollector.run(root) : ResourceCollector.run();
 			
@@ -54,17 +64,61 @@ public class Synthesis {
 				System.out.println("No HDL files found!");
 			}
 			else {
-				System.out.print("HDL souces: ");
-				System.out.println(collector.getSize());
+				m_hdlfile_ws_path = ResourcesPlugin.getWorkspace().getRoot().getLocation().toOSString();
+				
+			    File xst_prj_file;
+			    xst_prj_file = new File(m_hdlfile_ws_path, "nexushdl.prj");
+			    
+			    if (!xst_prj_file.exists())
+			    	xst_prj_file.createNewFile();
+			    else {
+			    	xst_prj_file.delete();
+			    	xst_prj_file.createNewFile();
+			    }
+			    
+			    FileWriter fstream = new FileWriter(xst_prj_file);
+		        BufferedWriter out = new BufferedWriter(fstream);
+				
+				Iterator<IResource> it = collector.iterator();
+				
+				while (it.hasNext()) {
+					
+					m_hdlfile = it.next();
+					m_hdlfile_path = m_hdlfile.getFullPath().toOSString();
+					m_hdlfile_extension = m_hdlfile.getFileExtension();
+					
+					if (m_hdlfile_extension.startsWith("vhd"))
+						m_hdlfile_type = "vhdl";
+					else
+						m_hdlfile_type = "verilog";
+					
+					System.out.printf("%s work %s\r\n", m_hdlfile_type, m_hdlfile_path);
+			        out.write(m_hdlfile_type);
+			        out.write(" work .");
+			        out.write(m_hdlfile_path);
+			        out.newLine();
+
+				}
+		        out.close();
 			
-				String[]	command = new String[3];
+				String[]	command = new String[7];
 				
 				command[0] = "/opt/Xilinx/12.1/ISE_DS/ISE/bin/lin/xst";
 				command[1] = "-ifn";
-				command[2] = "/home/michael/Projects/runtime-EclipseApplication/nexushdl.xst";
-				//command[3] =  "-instyle";
-				//command[4] = file.getLocation().makeAbsolute().toOSString();
-		
+				
+				StringBuffer synthesisfile_location = new StringBuffer(0);
+				synthesisfile_location.insert(0, m_hdlfile_ws_path);
+				synthesisfile_location.append("/nexushdl.xst");
+				command[2] = synthesisfile_location.toString();
+
+				StringBuffer synthesis_logfile_location = new StringBuffer(0);
+				synthesis_logfile_location.insert(0, m_hdlfile_ws_path);
+				synthesis_logfile_location.append("/nexushdl.srp");
+				command[3] = "-ofn";
+				command[4] = synthesis_logfile_location.toString();
+				
+				command[5] = "-intstyle";
+				command[6] = "xflow";
 			
 				ProcessBuilder pb = new ProcessBuilder(command);
 				Process p = pb.start();
